d2521038f7fe2b62bc7b8a03cda5386c57d6b636
Kjell Wooding <kjell@wooding.org>
2021-05-18 16:22:28 -0400

    Initial Easydata Generation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='1.5.0',
+    description='''Tools for debugging troublesome conda/pip environments''',
+    author='Kjell Wooding',
+    license='MIT',
+)
